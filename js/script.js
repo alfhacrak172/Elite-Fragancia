e21efe3573e8e625215f62568e1af0242c253a62
alfhacrak172 <fabriciovargas349@gmail.com>
2026-07-20 20:55:36 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Elite Fragancia</title>
+
+<link rel="stylesheet" href="css/estilos.css">
+
+</head>
+
+<body>
+
+<header>
+
+<div class="logo">
+
+<h1>ELITE FRAGANCIA</h1>
+
+<p>Decants Premium</p>
+
+</div>
+
+<nav>
+
+<a href="#inicio">Inicio</a>
+
+<a href="#perfumes">Perfumes</a>
+
+<a href="#contacto">Contacto</a>
+
+</nav>
+
+</header>
+
+<section id="inicio">
+
+<h2>Fragancias Originales</h2>
+
+<p>Calidad, elegancia y exclusividad en cada decant.</p>
+
+<a class="boton" href="#perfumes">Ver catálogo</a>
+
+</section>
+
+<section id="perfumes">
+
+<h2>Nuestros Perfumes</h2>
+
+</section>
+
+<section id="contacto">
+
+<h2>Contacto</h2>
+
+<p>📞 977 909 520</p>
+
+<p>🚚 Envíos solo a Pisco</p>
+
+<p>
+
+<a href="https://www.facebook.com/share/196XQh884D/" target="_blank">
+
+Facebook
+
+</a>
+
+</p>
+
+</section>
+
+<script src="js/script.js"></script>
+
+</body>
+
+</html>
